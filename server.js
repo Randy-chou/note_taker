48cf9a9ad4d593d57db9a03d8ea8689898a73167
Randy Chou <Randychou2020@gmail.com>
2021-07-01 00:09:54 -0400

    got index.html to use stylesheet and script using get requests from server

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use(express.json());
 
 // Routes
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, 'public/assets/css/styles.css')));
+
+app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, 'public//assets/js/index.js')));
+
 // Data read and write to database
 // fs.readFile('./db/db.json', 'utf8', (error, data) =>
 //   error ? console.error(error) : console.log(JSON.parse(data))
